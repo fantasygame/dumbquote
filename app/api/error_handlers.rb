9fab14762409713471a08982f3e0c362ab23fad7
Jakub Kubacki <kubacki.jk@gmail.com>
2017-07-19 00:52:51 +0700

    Comment out rollbar handlers

--- app/api/error_handlers.rb
+++ app/api/error_handlers.rb
@@ -7,38 +7,38 @@ module ErrorHandlers
   included do
     # global handler for simple not found case
     rescue_from ActiveRecord::RecordNotFound do |e|
-      Rollbar.warning(e)
+      # Rollbar.warning(e)
       return_error!("Record not found", 404)
     end
 
     rescue_from PG::UniqueViolation do |e|
-      Rollbar.warning(e)
+      # Rollbar.warning(e)
       return_error!("Duplicate key value violates unique constraint", 409)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      Rollbar.error(e)
+      # Rollbar.error(e)
       return_error!(e.message, 400)
     end
 
     rescue_from :all do |e|
-      Rollbar.error(e)
+      # Rollbar.error(e)
       status = e.try(:status) || 500
       return_error!(e.message, status, e)
     end
 
     rescue_from ActiveRecord::InvalidForeignKey do |e|
-      Rollbar.error(e)
+      # Rollbar.error(e)
       return_error!("Invalid Foreign Key", 500)
     end
 
     rescue_from Grape::Exceptions::ValidationErrors do |e|
-      Rollbar.error(e)
+      # Rollbar.error(e)
       return_error!(e.message, 400)
     end
 
     rescue_from Pundit::NotAuthorizedError do |e|
-      Rollbar.error(e)
+      # Rollbar.error(e)
       return_error!("Action unathorized", 403)
     end
   end
