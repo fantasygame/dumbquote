34c930cbe29db259a62c29d5729808dd7ec5842c
Jakub Kubacki <kubacki.jk@gmail.com>
2017-07-20 21:49:42 +0700

    Handle QUOTE_CREATED action in quotes reducer

--- app/javascript/packs/reducers/quotes.js
+++ app/javascript/packs/reducers/quotes.js
@@ -1,10 +1,14 @@
 import Constants from "../constants"
 
 const initialState = {
+  quotes: []
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
+    case Constants.QUOTE_CREATED:
+      state.quotes.push(action.createdQuote);
+      return { ...state, quotes: state.quotes };
     default:
       return state;
   }
