6027229db19058554b4b11fdaf2dd010cab07cc7
Jakub Kubacki <kubacki.jk@gmail.com>
2017-07-20 20:58:37 +0700

    Add dumb quote data to store

--- app/javascript/packs/reducers/quotes.js
+++ app/javascript/packs/reducers/quotes.js
@@ -1,6 +1,7 @@
 import Constants from "../constants"
 
 const initialState = {
+  quotes: [{ content: "quote1" }, { content: "quote2" }]
 };
 
 export default function reducer(state = initialState, action = {}) {
