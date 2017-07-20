5c107594c20986f61580e853e413bbb4053d1bd4
Jakub Kubacki <kubacki.jk@gmail.com>
2017-07-20 23:14:23 +0700

    Add axios config

--- app/javascript/packs/api/base.js
+++ app/javascript/packs/api/base.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: '/api/v1',
   headers: {
-    Accept: 'application/json',
+    Accept: 'application/vnd.api+json',
     'Content-Type': 'application/vnd.api+json',
   },
 });
